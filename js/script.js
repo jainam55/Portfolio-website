41862d67e94424c741f8241de8cda66e966ae129
Jainam Shah <jainam55@yahoo.co.in>
2019-02-19 12:54:50 -0500

    Modified and added skills and About me section

--- js/script.js
+++ js/script.js
@@ -6,4 +6,16 @@ $(document).ready(function(){
 		play:'6000',
 		pagination: false
 	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer", "Web Developer", "Student"],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+
+	});
+
+
+
 });
