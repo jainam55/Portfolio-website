41087294f2950628c514963fce2e3dece37798c3
Jainam Shah <jainam55@yahoo.co.in>
2019-02-27 22:10:25 -0500

    Made changes to faulty image display and adding stats section

--- js/script.js
+++ js/script.js
@@ -1,49 +1,47 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 	$('#slides').superslides({
-
-		animation:'fade',
-		play: 6000,
+		animation: 'fade',
+		play: 5000,
 		pagination: false
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineer", "Web Developer", "Student"],
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
 		showCursor: false
-
 	});
+
 	$('.owl-carousel').owlCarousel({
-    loop:true,
-    items: 4,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        768:{
-            items:3
-        	},
-        938:{
-            items:4
-        	}
-
-    	}
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
 	});
+
 
 	
+
 
 	var skillsTopOffset = $(".skillsSection").offset().top;
 
 	$(window).scroll(function() {
-		
-		console.log(window.pageYOffset);
 
-		if(window.pageYOffset > skillsTopOffset - $(window).height() + 300) {
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
 
 			$('.chart').easyPieChart({
 		        easing: 'easeInOut',
@@ -65,3 +63,19 @@ $(document).ready(function(){
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
